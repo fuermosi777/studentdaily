1731354440b53a7f5798530ac5a4b4cf70acfd2e
Hao Liu <liuhao1990@gmail.com>
2014-12-24 08:02:14 -0500

    Initial Commit

---
+++ studentdaily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  studentdaily
+//
+//  Created by Hao Liu on 12/24/14.
+//  Copyright (c) 2014 Hao Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
